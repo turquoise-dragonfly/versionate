ac78011a0aea136951f89f3135a1ca3cf7471284
Turquoise Dragonfly Studio <turquoise.dragonfly.studio@gmail.com>
2023-10-31 12:04:44 +0100

    resolves #3

--- src/entity/project.ts
+++ src/entity/project.ts
@@ -1,16 +1,53 @@
-import { existsSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
+import { string } from "yargs";
+
+export interface VersionFile {
+    path: string;
+    type: string;
+    location: string;
+}
 
 export class Project {
     file: string = '';
+    files: VersionFile[] = [];
 
     constructor(dirPath: string) {
         this.file = path.resolve(dirPath ?? process.cwd(), '.versionate');
+        if (existsSync(this.file)) {
+            const content = JSON.parse(readFileSync(this.file, 'utf8'));
+            this.files = content.files;
+        }
+    }
 
+    init() {
         if (existsSync(this.file)) {
             throw Error('Ya existe un proyecto en esta ubicación');
         }
+        this.save();
+    }
 
-        writeFileSync(this.file, '{}');
+    track(file: VersionFile) {
+        file.path = path.resolve(file.path);
+        if (!existsSync(file.path)) {
+            throw Error('El fichero no existe');
+        }
+        if (this.files.find(f => f.path === file.path)) {
+            throw Error('El fichero ya está siendo gestionado');
+        }
+        this.files.push(file);
+        this.save();
+        console.debug('Fichero de versión añadido al proyecto');
+    }
+
+    untrack(filePath: string) {
+        this.files = this.files.filter(f => f.path !== path.resolve(filePath));
+        this.save();
+    }
+
+    private save() {
+        writeFileSync(this.file, JSON.stringify({
+            "files": this.files
+        }));
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,34 @@ yargs(hideBin(process.argv))
 .command('init', 'Init project', (yargs) => {
     return yargs;
 }, async (argv) => {
-    new Project(process.cwd());
+    (new Project(process.cwd())).init();
+})
+.command('track path type location', 'Track a new file or update if currently tracked', (yargs) => {
+    return yargs
+    .positional('path', {
+        describe: 'Ruta del fichero a añadir'
+    })
+    .positional('type', {
+        describe: 'Tipo de fichero'
+    })
+    .positional('location', {
+        describe: 'Localización de la versión'
+    });
+}, async (argv) => {
+    (new Project(process.cwd())).track({
+        path: String(argv.path),
+        type: String(argv.type),
+        location: String(argv.location)
+    });
+})
+
+.command('untrack path', 'Track a new file or update if currently tracked', (yargs) => {
+    return yargs
+    .positional('path', {
+        describe: 'Ruta del fichero a eliminar'
+    });
+}, async (argv) => {
+    (new Project(process.cwd())).untrack(String(argv.path));
 })
 .option('verbose', {
     alias: 'v',
